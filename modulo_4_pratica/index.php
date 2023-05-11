999ae9389b76f1ca3866917175dc82d3a38d1053
Ruan <ruanlib.2003@gmail.com>
2023-05-11 14:52:30 -0300

    Corrigindo Bind Mount (nao atualizava)

--- modulo_4_pratica/index.php
+++ modulo_4_pratica/index.php
@@ -7,7 +7,7 @@
     <title>Mensagem</title>
 </head>
 <body>
-    <h1>Escreva uma mensagem</h1>
+    <h1>Escreva uma mensagem - Bind Mount</h1>
     <form action="process.php" method="POST">
         <input type="text" name="message" id="message">
         <input type="submit" value="Enviar">
